b805cd1bc94efeb6de22b479075c28481172bb4a
Long Nguyen <long.nguyen@mingle.com>
2019-04-05 10:13:16 +0700

    go go

---
+++ app/src/main/java/com/namdinh/cleanarchitecture/presentation/main/CheckStyleTest.java
@@ -0,0 +1,19 @@
+package com.namdinh.cleanarchitecture.presentation.main;
+
+
+import com.namdinh.cleanarchitecture.presentation.base.activity.BaseActivity;
+
+import android.os.storage.OnObbStateChangeListener;
+
+public class CheckStyleTest extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected int getNavControllerId() {
+        return 0;
+    }
+}
