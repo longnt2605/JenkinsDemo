57be33958d4076c14f557aac5eb5968be0200f8d
Long Nguyen <long.nguyen@mingle.com>
2019-04-05 10:23:34 +0700

    go go

--- app/src/main/java/com/namdinh/cleanarchitecture/presentation/main/CheckStyleTest.java
+++ app/src/main/java/com/namdinh/cleanarchitecture/presentation/main/CheckStyleTest.java
@@ -3,11 +3,9 @@ package com.namdinh.cleanarchitecture.presentation.main;
 
 import com.namdinh.cleanarchitecture.presentation.base.activity.BaseActivity;
 
-import android.os.storage.OnObbStateChangeListener;
-import android.os.storage.OnObbStateChangeListener;
-import android.os.storage.OnObbStateChangeListener;
-
 public class CheckStyleTest extends BaseActivity {
+
+    private static final String gdiuqwgdugqwudgiuqwgduwqgdugwqudgqwudguwqgduwqgduwgqudgwqudguwqgduqwgdugwqudgqwu2371287382173921793 = "gdiuqwgdugqwudgiuqwgduwqgdugwqudgqwudguwqgduwqgduwgqudgwqudguwqgduqwgdugwqudgqwu2371287382173921793";
 
     @Override
     protected int getLayoutId() {
